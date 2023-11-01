459bfa530fc140cdc08660d520bb16343cd19dba
temmir <to21zm@brocku.ca>
2023-11-01 01:11:54 -0400

    Create cc3script.js

---
+++ Lab8/cc3script.js
@@ -0,0 +1,6 @@
+function theRest(){
+  document.getElementById("output1").innerHTML="Output Zero Here";
+  document.getElementById("output2").innerHTML="Output One Here";
+  document.getElementById("output3").innerHTML="Outout Two Here";
+  document.getElementById("output4").innerHTML="Output Three Here";
+}
