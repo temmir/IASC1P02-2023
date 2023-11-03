68f94651aeb21c6ae382bbbadef42172c23c9d4f
temmir <to21zm@brocku.ca>
2023-11-02 21:23:35 -0400

    Create cc2scripts.js

---
+++ Lab8/cc2scripts.js
@@ -0,0 +1,17 @@
+//Sets the initial colours of the three elements on page load
+document.getElementById("blue").style.color ="blue";
+document.getElementById("green").style.color ="green";
+document.getElementById("red").style.color ="red";
+
+//A function to change colours of elements when called
+function mix(){
+
+	var blue = "#00BB00";
+	var green ="#BB9320";
+	var red = "#AB00FF";
+
+	document.getElementById("blue").style.color=blue;
+	document.getElementById ("green").style.color=green;
+	document.getElementById("red").style.color=red;
+
+}
